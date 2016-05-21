a919d85ac7df467d7ba01ff7fc981c181eece926
Chen Rotem Levy <chen@rotemlevy.name>
2016-05-21 15:47:43 +0300

    Bank tested

--- bank.py
+++ bank.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import pickle
-import atm.account
+from .account import Account
+
+class BankException(Exception):
+    pass
 
 class Bank(object):
 
-    def __init__(self):
+    def __init__(self, store):
+        self.store = store
         self.accounts = {}
 
-    def load(self, file):
-        pass
+    def load(self):
+        self.accounts = {id:int(balance) for id,balance in self.store}
 
-    def save(self, file):
-        pass
+    def save(self):
+        self.store(self.accounts.iteritems())
 
-    def account(self, id):
+    def __enter__(self):
+        self.load()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.save()
+
+    def get_account(self, id):
         try:
-            return self.accounts[id]
-        except IndexError:
-            raise account.AccountException('not exists')
+            return Account(id, self.accounts[id])
+        except KeyError:
+            raise BankException('not exists', id)
+
+    def put_account(self, account):
+        self.accounts[account.id] = account.balance
---
+++ tests/test_bank.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from atm.bank import Bank, BankException
+from atm.account import Account
+
+LIST_CONTENT = [('new_account', 0), ('account_100', 100)]
+DICT_CONTENT = { 'new_account': 0, 'account_100': 100 }
+
+class MockStore(object):
+    def __iter__(self):
+        return iter(LIST_CONTENT)
+    def __call__(self, it):
+        self.saved = list(it)
+
+@pytest.fixture
+def some_bank(some_store):
+    return Bank(MockStore())
+
+@pytest.fixture
+def loaded_bank(some_bank):
+    some_bank.load()
+    return some_bank
+
+
+def test_bank_creation():
+    bank = Bank('store')
+    assert bank.store == 'store'
+    assert bank.accounts == {}
+
+def test_bank_load(some_bank):
+    some_bank.load()
+    assert some_bank.accounts == DICT_CONTENT
+
+def test_bank_save(some_bank):
+    some_bank.accounts = DICT_CONTENT
+    some_bank.save()
+    assert some_bank.store.saved == LIST_CONTENT
+
+def test_bank_load_save(loaded_bank):
+    loaded_bank.save()
+    assert loaded_bank.store.saved == LIST_CONTENT
+
+def test_bank_context(some_bank):
+    with some_bank:
+       assert some_bank.accounts == DICT_CONTENT
+    assert some_bank.store.saved == LIST_CONTENT
+
+def test_get_account(loaded_bank):
+    account = loaded_bank.get_account('account_100')
+    assert account.id == 'account_100'
+    assert account.balance == 100
+
+def test_get_account_exception(loaded_bank):
+    with pytest.raises(BankException):
+        loaded_bank.get_account('non_exist_account')
+
+def test_put_account(loaded_bank):
+    account = Account('non_exists_account', 42)
+    loaded_bank.put_account(account)
+    assert loaded_bank.accounts['non_exists_account'] == 42
+
+def test_assert_put_get(loaded_bank):
+    put_acc = Account('non_exists_account', 42)
+    loaded_bank.put_account(put_acc)
+    get_acc = loaded_bank.get_account('non_exists_account')
+    assert get_acc.id == 'non_exists_account'
+    assert getattr.balance == 42
